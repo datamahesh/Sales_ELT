0b5a7e9a18ba2c690fe529b2b39e893eae3be34f
Mahesh <127187181+datamahesh@users.noreply.github.com>
2025-03-22 18:04:53 +0530

    Create Silver_ERP_LOC_A101.sql

---
+++ dbt_cloud/Models/Silver/Silver_ERP_LOC_A101.sql
@@ -0,0 +1,12 @@
+with Silver_ERP_LOC_A101 as (
+select
+    REPLACE(cid, '-', '') AS cid, 
+			CASE
+				WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+				ELSE TRIM(cntry)
+			END AS cntry -- Normalize and Handle missing or blank country codes
+		FROM {{ref("Bronze_ERP_LOC_A101")}}
+)
+select * from Silver_ERP_LOC_A101
