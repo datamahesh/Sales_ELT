e8e3d9b9322a1099f89ce11bd69f2ecffbdcb451
Mahesh <127187181+datamahesh@users.noreply.github.com>
2025-03-22 18:01:16 +0530

    Create Gold_Fact_Sales.sql

---
+++ dbt_cloud/Models/Gold/Gold_Fact_Sales.sql
@@ -0,0 +1,18 @@
+with Gold_Fact_Sales as (
+    SELECT
+    sd."sls_ord_num"  AS order_number,
+    pr.product_key  AS product_key,
+    cu.customer_key AS customer_key,
+    sd."sls_order_dt" AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt  AS due_date,
+    sd.sls_sales   AS sales_amount,
+    sd."sls_quantity" AS quantity,
+    sd.sls_price   AS price
+FROM {{ref('Silver_CRM_SALES_DETAILS')}} sd
+LEFT JOIN {{ref('Gold_Dim_Products')}} pr
+    ON sd."sls_prd_key" = pr.product_number
+LEFT JOIN {{ref('Gold_Dim_Customers')}} cu
+    ON sd."sls_cust_id" = cu.customer_id  
+)
+select * FROM  Gold_Fact_Sales
